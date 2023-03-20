a724539fda63588ae21465580073ba385c0fab98
Brandon Yi <yimail.brandon@gmail.com>
2023-03-20 12:45:19 -0700

    Reading data in tile map layer2 to determine where spawn locations are
    Using different layers of tile map to describe which elements are spawn locations

--- src/server/levels/level.js
+++ src/server/levels/level.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 
 class LevelDescription {
-    constructor (height, width, tileMap) {
+    constructor (height, width, tileMap, spawnPointId) {
         this.height = height
         this.width = width
         this.tileMap = tileMap
+        this.spawnPointId = spawnPointId
 
         const data = fs.readFileSync(tileMap)
         this.tileMapData = JSON.parse(data)
@@ -12,14 +13,32 @@ class LevelDescription {
         this.spawnPoints = this._defineSpawnPoints()
     }
 
+    getSpawnPoints () {
+        return this.spawnPoints
+    }
+
     _defineSpawnPoints () {
         // by convention. layer 1 contains spawn point locations
         const layer2 = this.tileMapData.layers[1]
-        console.log(layer2)
-        return []
+        const tileData = layer2.data
+
+        const spawnPoints = []
+        // x values are columns, y values are rows
+        // y starts at 0 and decreases as you go down
+        for (let y = 0; y > -this.height; y--) {
+            for (let x = 0; x < this.width; x++) {
+                const tileIndex = (Math.abs(y) * this.width) + x
+                const tileId = tileData[tileIndex]
+                if (tileId === this.spawnPointId) {
+                    spawnPoints.push({ x: x, y: y })
+                }
+            }
+        }
+        
+        return spawnPoints
     }
 }
 
-const simplePlane = new LevelDescription(15, 15, './src/resources/plane.json')
+const simplePlane = new LevelDescription(15, 15, './src/resources/plane.json', 26)
 
 //module.exports = LevelDescription
