d993f6a7abc9a7a3b8f577470fcaa8f15d5d0999
Brandon Yi <yimail.brandon@gmail.com>
2023-04-16 17:51:11 -0700

    Added test case for Fallback control node (fizz buzz)

--- test/behaviorTree_UnitTest.js
+++ test/behaviorTree_UnitTest.js
@@ -52,6 +52,10 @@ describe('BehaviorTree', () => {
 
     const tree = new BehaviorTree(context)
     const inverter = new Decorator((status) => {
+      if (status === nodeStatus.RUNNING) {
+        return nodeStatus.RUNNING
+      }
+
       return status === nodeStatus.SUCCESS ? nodeStatus.FAILURE : nodeStatus.SUCCESS
     })
     const sequence = new Sequence()
@@ -81,5 +85,100 @@ describe('BehaviorTree', () => {
 
     await p
     chai.expect(tree.tick()).to.be.equal(nodeStatus.SUCCESS)
+  })
+
+  it('simple case with fallback executes as expected', async () => {
+    // fizz buzz but with a behavior tree
+    function getFizzBuzzSequence(max) {
+      let fb = []
+      for (let i = 1; i <= max; i++) {
+        fb.push(fizzBuzz(i))
+      }
+
+      return fb
+    }
+
+    function fizzBuzz(n) {
+      if (n % 3 === 0 && n % 5 === 0) {
+        return 'fizzbuzz'
+      } else if (n % 3 === 0) {
+        return 'fizz'
+      } else if (n % 5 === 0) {
+        return 'buzz'
+      } else {
+        return n
+      }
+    }
+
+    const context = {
+      counter: 1,
+      fizzBuzz: []
+    }
+
+    const tree = new BehaviorTree(context)
+
+    const root = new Fallback()
+
+    const doesSayFizz = new Sequence()
+    const isCounterDivisibleByThree = new Condition((context) => {
+      return context.counter % 3 === 0
+    })
+    const fizz = new Action(async (context) => {
+      context.fizzBuzz.push('fizz')
+      context.counter++
+    })
+
+    doesSayFizz.addChild(isCounterDivisibleByThree)
+    doesSayFizz.addChild(fizz)
+
+    const doesSayBuzz = new Sequence()
+    const isCounterDivisibleByFive = new Condition((context) => {
+      return context.counter % 5 === 0
+    })
+    const buzz = new Action(async (context) => {
+      context.fizzBuzz.push('buzz')
+      context.counter++
+    })
+
+    doesSayBuzz.addChild(isCounterDivisibleByFive)
+    doesSayBuzz.addChild(buzz)
+
+    const doesSayFizzBuzz = new Sequence()
+    const isCounterDivisibleByThreeAndFive = new Condition((context) => {
+      return context.counter % 3 === 0 && context.counter % 5 === 0
+    })
+    const fizzbuzz = new Action(async (context) => {
+      context.fizzBuzz.push('fizzbuzz')
+      context.counter++
+    })
+
+    doesSayFizzBuzz.addChild(isCounterDivisibleByThreeAndFive)
+    doesSayFizzBuzz.addChild(fizzbuzz)
+
+    const sayNumber = new Action(async (context) => {
+      context.fizzBuzz.push(context.counter)
+      context.counter++
+    })
+
+    root.addChild(doesSayFizzBuzz)
+    root.addChild(doesSayBuzz)
+    root.addChild(doesSayFizz)
+    root.addChild(sayNumber)
+
+    tree.setRoot(root)
+
+    const p = new Promise((resolve, reject) => {
+      const ticker = setInterval(() => {
+        if (context.counter >= 100) {
+          clearInterval(ticker)
+          resolve()
+        }
+
+        tree.tick()
+      }, 1000/60)
+    })
+
+    await p
+    chai.expect(context.fizzBuzz).to.be.deep.equal(getFizzBuzzSequence(100))
   })
 })
