32a93bff19e714f7bb1b4fb72a7491dee81ce1b4
Brandon Yi <yimail.brandon@gmail.com>
2023-08-18 17:17:50 -0700

    Added endpoint for users to submit feedback
    Also added an integration test to ensure behavior of new endpoint

---
+++ src/server/routes/feedbackRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const services = require('../services/services')
+
+router.post('/submit', async (req, res) => {
+  const { feedback } = req.body
+
+  try {
+    await services.feedback.submitFeedback(feedback)
+    res.status(200).send('Feedback submitted')
+  } catch (err) {
+    if (err.name === 'InvalidFeedbackError') {
+      res.status(400).send('Invalid feedback')
+    } else {
+      res.status(500).send('Internal server error')
+    }
+  }
+})
+
+module.exports = router
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -2,8 +2,10 @@ const passport = require('passport')
 
 const gameSession = require('./gameSessionRoutes')
 const user = require('./userRoutes')
+const feedback = require('./feedbackRoutes')
 
 module.exports = (app) => {
     app.use('/api/game-session', passport.authenticate('jwt'), gameSession)
     app.use('/api/user', user)
+    app.use('/api/feedback', feedback)
 }
