e3b35c4ae977c3c84ad844da160798abf2f221fa
Brandon Yi <yimail.brandon@gmail.com>
2023-08-26 19:19:15 -0700

    Added spinners above JKL buttons
    Spinners indcate that state change is on cooldown

--- src/gameview/public/components/stateChangeIndicators.js
+++ src/gameview/public/components/stateChangeIndicators.js
@@ -11,19 +11,42 @@ class StateChangeIndicators extends Component {
 
     return `
       <div class="state-change-indicators" data-cy="state-change-indicators">
-        <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/j_key_rock.png"></image>
-        <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/k_key_paper.png"></image>
-        <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/l_key_scissors.png"></image>
+        <div class="indicator">
+            <div class="spinner ${isOnCoolDown ? '' : 'spinner-invisible'}"></div>
+            <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/j_key_rock.png"></image>
+        </div>
+        <div class="indicator">
+            <div class="spinner ${isOnCoolDown ? '' : 'spinner-invisible'}"></div>
+            <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/k_key_paper.png"></image>
+        </div>
+        <div class="indicator">
+            <div class="spinner ${isOnCoolDown ? '' : 'spinner-invisible'}"></div>
+            <image class="key ${isOnCoolDown ? 'cooldown' : ''}" src="/assets/l_key_scissors.png"></image>
+        </div>
       </div>
+      <style>
+        @-webkit-keyframes spin {
+          0% { -webkit-transform: rotate(0deg); }
+          100% { -webkit-transform: rotate(360deg); }
+        }
+
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      </style>
     `
   }
 
   getStyleMap() {
     return {
       ...super.getStyleMap(),
-      'state-change-indicators': 'position: absolute; bottom: 0; left: 0; display: flex; z-index: 1000;',
-      'key': 'width: 100px; height: 100px; margin: 0 10px;',
-      'cooldown': 'width: 100px; height: 100px; margin: 0 10px; opacity: 0.5'
+      'state-change-indicators': 'position: absolute; bottom: 0; left: 0; display: flex; z-index: 1000; margin-bottom: 10px;',
+      'indicator': 'position: relative; display: flex; flex-direction: column; align-items: center; justify-content: center; margin: 0 10px;',
+      'spinner': 'opacity: 0.75; border: 16px solid #f3f3f3; border-top: 16px solid #a3a3a3; border-radius: 50%; width: 50px; height: 50px; animation: spin 0.5s linear infinite; z-index: 1000; -webkit-animation: spin 0.5s linear infinite; position: absolute;',
+      'spinner-invisible': 'display: none;',
+      'key': 'width: 100px; height: 100px; margin: 0 10px; position: relative;',
+      'cooldown': 'width: 100px; height: 100px; margin: 0 10px; opacity: 0.5; position: relative;'
     }
   }
 }
